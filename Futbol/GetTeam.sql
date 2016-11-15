352b2a1f0133c7db162dff169d73180ad100126a
Brad <bradleymcmanus@hotmal.com>
2016-11-14 18:51:10 -0600

    init

---
+++ Futbol/GetTeam.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[GetTeam]
+@TeamID int
+AS
+	SET NOCOUNT ON;
+
+	SELECT *
+	FROM dbo.Team
+	WHERE @TeamID = @TeamID;  --WHERE TeamID = @TeamID;
+GO
---
+++ Futbol/Team.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Team]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	TeamName nvarchar(255) Not Null,
+	City nvarchar(50) NOT NULL,
+	Country nvarchar(50)  NOT NULL,
+	CONSTRAINT UQ_TeamName UNIQUE (TeamName)
+)
+go
+create nonclustered index IX_Team_City on dbo.Team(City)
+go
